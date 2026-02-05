5c1d30cf038433339014889a8c4094e55b1316b5
Potluri-11 <srujanapotluri43@gmail.com>
2026-02-05 16:10:11 +0530

    Update and rename variables.tf to variables.tf

---
+++ terraform_ec2/variables.tf
@@ -0,0 +1,19 @@
+variable "ami_id" {
+  description = "AMI ID to use for the EC2 instance"
+  type        = string
+}
+
+variable "instance_type" {
+  description = "EC2 instance type"
+  type        = string
+}
+
+variable "key_name" {
+  description = "SSH key pair name"
+  type        = string
+}
+
+variable "instance_name" {
+  description = "Name tag for EC2 instance"
+  type        = string
+}
